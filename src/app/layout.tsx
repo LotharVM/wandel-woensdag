d80983e763d866862455bb4095afb2819b1d1102
Lothar van Mourik <lothar@bravoure.nl>
2023-11-07 08:47:12 +0100

    fix: ts errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,7 @@ import './global.css';
 import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
-import serverContext from '@/utils/serverContext';
 import { Animate } from '@/components/Animate';
-import { css } from 'styled-system/css';
-
-export const [getPageSlug, setPageSlug] = serverContext('/');
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
   return (
