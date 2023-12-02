7d06e7cb5aa09b3364e05b14278ffb06901824c5
Lothar van Mourik <lothar@bravoure.nl>
2023-12-02 17:54:54 +0100

    fix

--- src/components/MotionDiv.jsx
+++ src/components/MotionDiv.jsx
@@ -1,6 +1,7 @@
 import { motion } from 'framer-motion';
 import React from 'react';
 
+// eslint-disable-next-line react/display-name
 const Component = React.forwardRef((props, ref) => <div ref={ref} />);
 
 export const MotionDiv = motion(Component);
